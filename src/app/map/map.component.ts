0d685a5e75468d8a437f47cbfd28dfdf0984a3ff
Adiyamseged Nerri <adiyamgn@outlook.com>
2017-08-01 15:29:03 -0400

    Implemented the sample gpx data in using mapbox and mapbox omnivore in the map component.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
-
 import { MapService } from '../services/map.service';
 import { IActivity } from '../shared/activity.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-
 export class MapComponent implements OnInit {
 
-  constructor(private _mapService: MapService, private _route: ActivatedRoute) {
-
-  }
+  constructor(private _mapService: MapService,
+              private _route: ActivatedRoute) { }
 
   activity: any;
   activityName: string;
-  activityDate: Date;
   activityNotes: string;
+  activityDate: Date;
   activityDistance: number;
   gpx: any;
 
   ngOnInit() {
     this.activity = this._mapService.getActivity(
       +this._route.snapshot.params['id'])
-    )
   }
+
+  ngAfterViewInit(){
+    this._mapService.plotActivity(+this._route.snapshot.params['id']);
+    this.activityName = this.activity.name;
+    this.activityNotes = this.activity.notes;
+    this.activityDistance = this.activity.distance;
+    this.activityDate = this.activity.date;
+    this.gpx = this.activity.gpxData;
+  }
+
 }
